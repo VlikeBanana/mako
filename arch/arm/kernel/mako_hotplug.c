90dc1ee8b139750038ca9f777d94c7fe2491256b
labodj <jacopolabardi@gmail.com>
2013-06-05 17:16:18 +0200

    Fix compiling errors with gcc-4.7.

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -56,7 +56,7 @@ static void scale_interactive_tunables(unsigned int above_hispeed_delay,
     scale_min_sample_time(min_sample_time);
 }
 
-static void decide_hotplug_func(struct work_struct *work)
+static void __cpuinit decide_hotplug_func(struct work_struct *work)
 {
     int cpu;
     int cpu_boost;
@@ -122,7 +122,7 @@ static void decide_hotplug_func(struct work_struct *work)
     queue_delayed_work_on(0, wq, &decide_hotplug, msecs_to_jiffies(timer));
 }
 
-static void mako_hotplug_early_suspend(struct early_suspend *handler)
+static void __cpuinit mako_hotplug_early_suspend(struct early_suspend *handler)
 {	 
     int cpu;
 
@@ -147,7 +147,7 @@ static void mako_hotplug_early_suspend(struct early_suspend *handler)
             0, stats.suspend_frequency/1000);
 }
 
-static void mako_hotplug_late_resume(struct early_suspend *handler)
+static void __cpuinit mako_hotplug_late_resume(struct early_suspend *handler)
 {  
     int cpu;
 
@@ -170,7 +170,7 @@ static void mako_hotplug_late_resume(struct early_suspend *handler)
     queue_delayed_work_on(0, wq, &decide_hotplug, HZ);
 }
 
-static struct early_suspend mako_hotplug_suspend =
+static struct early_suspend __refdata mako_hotplug_suspend =
 {
     .level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN + 1,
 	.suspend = mako_hotplug_early_suspend,
