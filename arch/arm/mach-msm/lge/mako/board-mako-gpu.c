0cbf5a4e37ecd4e78a4619d2ffa67d4ae1182497
engstk <eng.stk@sapo.pt>
2013-08-22 18:22:06 +0100

    GPU OC tweaks III

--- arch/arm/mach-msm/lge/mako/board-mako-gpu.c
+++ arch/arm/mach-msm/lge/mako/board-mako-gpu.c
@@ -272,12 +272,12 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 3,
-			.io_fraction = 100,
+			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 200000000,
